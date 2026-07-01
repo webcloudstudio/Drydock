e6a9d50d736dc1f709b7a94e5614ca713ba13432
Ed Barlow <edward.m.barlow@gmail.com>
2026-06-30 23:05:15 -0400

    x

--- bin/publish_pypi.sh
+++ bin/publish_pypi.sh
@@ -1,6 +1,15 @@
+source /mnt/c/Users/barlo/projects/Drydock/.venv/bin/activate
+
+export UV_PUBLISH_TOKEN="$(awk '
+    $0=="[pypi]" {in_pypi=1; next}
+    /^\[/ && $0!="[pypi]" {in_pypi=0}
+    in_pypi && $1=="password" {print $3; exit}
+  ' ~/.pypirc)"
+# echo $UV_PUBLISH_TOKEN
+
 # uv build
 cd /mnt/c/Users/barlo/projects/Drydock
 rm -rf dist/
-python -m hatchling build
+python3 -m hatchling build
 unzip -l dist/drydock_sdd-*.whl
 uv publish dist/*
