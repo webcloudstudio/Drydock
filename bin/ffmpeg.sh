24f457ca1af4c2283a7026940b5406bc030cc8f2
Ed Barlow <edward.m.barlow@gmail.com>
2026-07-12 17:00:52 -0400

    Fix ffmpeg video scaling filter quoting

--- bin/ffmpeg.sh
+++ bin/ffmpeg.sh
@@ -1,2 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 cd /mnt/c/Users/barlo/projects/Drydock/docs
-ffmpeg     -i ./presentation/Drydock_Video.mp4     -vf scale=min(1920,iw):-2     -c:v libx264     -preset slow     -crf 28     -pix_fmt yuv420p     -movflags +faststart     -c:a aac     -b:a 96k ./presentation/Drydock_Video.web.mp4
+
+ffmpeg \
+    -i ./presentation/Drydock_Video.mp4 \
+    -vf 'scale=min(1920\,iw):-2' \
+    -c:v libx264 \
+    -preset slow \
+    -crf 28 \
+    -pix_fmt yuv420p \
+    -movflags +faststart \
+    -c:a aac \
+    -b:a 96k \
+    ./presentation/Drydock_Video.web.mp4
